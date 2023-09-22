06c1a86bfbf1643fa5c3d126bcff2814e5788b77
Kuznetsova Elena <kuzn.elena@yahoo.com>
2023-09-22 20:28:03 +0500

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Button from './components/Button/Button'
+import { MouseEvent } from 'react';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
+  // сonst [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (e: MouseEvent) => {
+    console.log(e);
+
+  };
+
+  // useEffect(() => {}, []);
 
   return (
     <>
-      <Button onClick={() => console.log()}>Кнопка</Button>
+      <Button onClick={addCounter}>Кнопка</Button>
+      <Button appearance='big' onClick={addCounter}>Кнопка</Button>
+      <Input placeholder='Email'/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
-import './Button.css';
+import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-function Button({children, className, ...props}: ButtonProps) {
+function Button({children, className, appearance = 'small', ...props}: ButtonProps) {
     
     return (
-        <button className={cn('button accent', className)} {...props}>{children}</button>
+        <button className={cn(styles['button'], styles['accent'], className, {
+            [styles['small']]: appearance === 'small',
+            [styles['big']]: appearance === 'big'
+        })} {...props}>{children}</button>
     );
 }
 
